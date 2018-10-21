3cf0f889f00531e40c68ee83e6edc9a8e489c5e9
beepscore <support@beepscore.com>
2018-10-20 22:42:22 -0700

    Add testTreeValidatorTree5381469. // TODO: fix isValid so test passes

--- BinarySearchTreeTests/TreeValidatorTests.swift
+++ BinarySearchTreeTests/TreeValidatorTests.swift
@@ -41,4 +41,20 @@ class TreeValidatorTests: XCTestCase {
                                             ancestorsMaxValue: 0))
     }
 
+    // TODO: fix isValid so test passes
+    func testTreeValidatorTree5381469() {
+        let testBundle = Bundle(for: type(of: self))
+
+        guard let node = TreeBuilder.tree(bundle: testBundle,
+                                          fileName: "tree5381469",
+                                          fileExtension: "json") else {
+                                            XCTFail()
+                                            return
+        }
+
+        XCTAssertTrue(TreeValidator.isValid(tree: node, nodeType: .root,
+                                            ancestorsMinValue: 0,
+                                            ancestorsMaxValue: 0))
+    }
+
 }
