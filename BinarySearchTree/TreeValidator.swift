197b31a8ca09b751eeb8850f4115b182454f6639
beepscore <support@beepscore.com>
2018-10-25 13:38:30 -0700

    Simplify isValid recursive calls, only need 2 not 4

--- BinarySearchTree/TreeValidator.swift
+++ BinarySearchTree/TreeValidator.swift
@@ -30,11 +30,11 @@ class TreeValidator {
     ///   - nodeType:
     ///   - floor: node value must be greater than floor.
     ///     The value of node's nearest ancestor that contains node in its right subtree.
-    ///     (reset whenever go path branches right)
+    ///     (reset to node's parent value whenever branch right)
     ///     ignored if nodeType is root
     ///   - ceiling: node value must be less than ceiling.
     ///     The value of node's nearest ancestor that contains node in its left subtree.
-    ///     (reset whenever path branches left)
+    ///     (reset to node's parent value whenever branch left)
     ///     ignored if nodeType is root
     /// - Returns: true if node is a valid binary search tree.
     ///   returns true if node is nil
@@ -50,74 +50,26 @@ class TreeValidator {
         // check node
 
         guard let node = node else {
-            // node is nil, we define nil as a valid tree
+            // sic- node is nil, we define nil as a valid tree
             print("node is nil, returning true")
             return true
         }
 
-        if !isNodeValueWithinFloorAndCeiling(node: node, floor: floor, ceiling: ceiling) {
+        if nodeType != .root
+            && !isNodeValueWithinFloorAndCeiling(node: node, floor: floor, ceiling: ceiling) {
             return false
         }
 
         // check node children
 
-        if let nodeLeft = node.left {
-
-            if !isNodeValueWithinFloorAndCeiling(node: nodeLeft, floor: floor, ceiling: node.value) {
-                return false
-            }
-
-            // check nodeLeft subtrees
-
-            // left, left => decrease ceiling, don't change floor
-            let isLeftLeftValid = isValid(node: nodeLeft.left,
-                                      nodeType: .left,
-                                      floor: floor,
-                                      ceiling: nodeLeft.value)
-            print("\(node.description()) isLeftLeftValid: \(isLeftLeftValid)")
-            if !isLeftLeftValid {
-                return false
-            }
-
-            // path switched direction, so update both floor and ceiling
-            let isLeftRightValid = isValid(node: nodeLeft.right,
-                                          nodeType: .right,
-                                          floor: nodeLeft.value,
-                                          ceiling: node.value)
-            print("\(node.description()) isLeftRightValid: \(isLeftRightValid)")
-            if !isLeftRightValid {
-                return false
-            }
-
+        // branch left, decrease ceiling
+        if !isValid(node: node.left, nodeType: .left, floor: floor, ceiling: node.value) {
+            return false
         }
 
-        if let nodeRight = node.right {
-
-            if !isNodeValueWithinFloorAndCeiling(node: nodeRight, floor: node.value, ceiling: ceiling) {
-                return false
-            }
-
-            // check nodeRight subtrees
-
-            // path switched direction, so update both floor and ceiling
-            let isRightLeftValid = isValid(node: nodeRight.left,
-                                          nodeType: .left,
-                                          floor: node.value,
-                                          ceiling: nodeRight.value)
-            print("\(node.description()) isRightLeftValid: \(isRightLeftValid)")
-            if !isRightLeftValid {
-                return false
-            }
-
-            // right, right => increase floor, don't change ceiling
-            let isRightRightValid = isValid(node: nodeRight.right,
-                                           nodeType: .right,
-                                           floor: nodeRight.value,
-                                           ceiling: ceiling)
-            print("\(node.description()) isRightRightValid: \(isRightRightValid)")
-            if !isRightRightValid {
-                return false
-            }
+        // branch right, increase floor
+        if !isValid(node: node.right, nodeType: .right, floor: node.value, ceiling: ceiling) {
+            return false
         }
 
         return true
